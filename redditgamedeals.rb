84451b98fbc679431f56ae0bb3e342a5b98460ee
fbrandy <adit293@ymail.com>
2015-08-31 07:35:09 +0000

    modified to switch-case

--- redditgamedeals.rb
+++ redditgamedeals.rb
@@ -9,49 +9,61 @@ puts "Reddit GameDeals"
 puts "=================="
 puts " "
 
-docnew = Nokogiri::HTML(open("https://www.reddit.com/r/gamedeals/new/.compact"))
-docnew.remove_namespaces!
-newdeals = docnew.xpath("//div[@class='content']/div[@id='siteTable']/div")
+print "What deals you want to see [recent/popular]? "
+deals = gets.chomp
 
-puts "5 recent deals"
-puts " "
-
-newdeals[0..4].each do |nd|
-    count+=1
-    puts nd.at_css('p/a').text
-    slink = nd.at_css("p[@class='title']/a/@href") # link from store
-    rlink = nd.at_css('@href') # reddit link
-    puts "Store link: #{slink.text}"
-    puts "Reddit thread link: #{rlink.text}"
-    puts nd.at_css("/div[@class='commentcount']/a").text + " comments" + " | " +
-         nd.at_css("/div[@class='entry unvoted']/div/span[1]/time")
-    time = nd.at_css("/div[@class='entry unvoted']/div/span[1]/time")
-    timeutc = nd.at_css("/div[@class='entry unvoted']/div/span[1]/time/@title")
-    puts "Time posted: #{time.text} | " + "#{timeutc.text}"
+case deals
+    when "recent"
+    docnew = Nokogiri::HTML(open("https://www.reddit.com/r/gamedeals/new/.compact"))
+    docnew.remove_namespaces!
+    newdeals = docnew.xpath("//div[@class='content']/div[@id='siteTable']/div")
+    
     puts " "
-end
-
-doc = Nokogiri::HTML(open("https://i.reddit.com/r/gamedeals"))
-doc.remove_namespaces!
-deals = doc.xpath("//div[@class='content']/div[@id='siteTable']/div")
-
-puts "Popular Deals"
-puts "============="
-puts " "
-
-deals[1..-1].each do |dl|
-    count+=1
-    puts dl.at_css('p/a').text
-    slink = dl.at_css("p[@class='title']/a/@href")
-    rlink = dl.at_css('@href')
-    puts "Store link: #{slink.text}"
-    puts "Reddit thread link: #{rlink.text}"
-    puts dl.at_css("div[@class='commentcount']/a").text + " comments" + " | " +
-         dl.at_css("/div[@class='entry unvoted']/div/span[1]/span")
-    time = dl.at_css("/div[@class='entry unvoted']/div/span[1]/time")
-    timeutc = dl.at_css("/div[@class='entry unvoted']/div/span[1]/time/@title")
-    puts "Time posted: #{time.text} | " + "#{timeutc.text}"
+    puts "5 recent deals"
+    puts "=============="
     puts " "
+
+    newdeals[0..4].each do |nd|
+        count+=1
+        puts nd.at_css('p/a').text
+        slink = nd.at_css("p[@class='title']/a/@href") # link from store
+        rlink = nd.at_css('@href') # reddit link
+        puts "Store link: #{slink.text}"
+        puts "Reddit thread link: #{rlink.text}"
+        puts nd.at_css("/div[@class='commentcount']/a").text + " comments" + " | " +
+        nd.at_css("/div[@class='entry unvoted']/div/span[1]/time")
+        time = nd.at_css("/div[@class='entry unvoted']/div/span[1]/time")
+        timeutc = nd.at_css("/div[@class='entry unvoted']/div/span[1]/time/@title")
+        puts "Time posted: #{time.text} | " + "#{timeutc.text}"
+        puts " "
+    end
+
+    when "popular"
+    doc = Nokogiri::HTML(open("https://i.reddit.com/r/gamedeals"))
+    doc.remove_namespaces!
+    deals = doc.xpath("//div[@class='content']/div[@id='siteTable']/div")
+    
+    puts " "
+    puts "Popular Deals"
+    puts "============="
+    puts " "
+
+    deals[1..-1].each do |dl|
+        count+=1
+        puts dl.at_css('p/a').text
+        slink = dl.at_css("p[@class='title']/a/@href")
+        rlink = dl.at_css('@href')
+        puts "Store link: #{slink.text}"
+        puts "Reddit thread link: #{rlink.text}"
+        puts dl.at_css("div[@class='commentcount']/a").text + " comments" + " | " +
+        dl.at_css("/div[@class='entry unvoted']/div/span[1]/span")
+        time = dl.at_css("/div[@class='entry unvoted']/div/span[1]/time")
+        timeutc = dl.at_css("/div[@class='entry unvoted']/div/span[1]/time/@title")
+        puts "Time posted: #{time.text} | " + "#{timeutc.text}"
+        puts " "
+    end
+    else
+        puts "Invalid choice"
 end
 
 puts "#{count} sales found"
--- redditpcsales.rb
+++ redditpcsales.rb
@@ -9,50 +9,67 @@ puts "Reddit Build-a-PC Sales"
 puts "======================="
 puts " "
 
-docnew = Nokogiri::HTML(open("https://www.reddit.com/r/buildapcsales/new/.compact"))
-docnew.remove_namespaces!
-newsales = docnew.xpath("//div[@class='content']/div[@id='siteTable']/div")
 
-puts "5 recent deals"
-puts " "
+print "What deals you want to see [recent/popular]? "
+deals = gets.chomp
 
-newsales[0..4].each do |nw|
-    count+=1
-    puts nw.at_css('p/a').text
-    slink = nw.at_css("p[@class='title']/a/@href")
-    link = nw.at_css('@href')
-    puts "Store link: #{slink.text}"
-    puts "Reddit thread link:  #{link.text}"
-    puts nw.at_css("div[@class='commentcount']/a").text + " comments | " + 
+
+case deals
+    when "recent"
+    
+    docnew = Nokogiri::HTML(open("https://www.reddit.com/r/buildapcsales/new/.compact"))
+    docnew.remove_namespaces!
+    newsales = docnew.xpath("//div[@class='content']/div[@id='siteTable']/div")
+    
+    puts " "
+    puts "5 recent deals"
+    puts "=============="
+    puts " "
+
+    newsales[0..4].each do |nw|
+        count+=1
+        puts nw.at_css('p/a').text
+        slink = nw.at_css("p[@class='title']/a/@href")
+        link = nw.at_css('@href')
+        puts "Store link: #{slink.text}"
+        puts "Reddit thread link:  #{link.text}"
+        puts nw.at_css("div[@class='commentcount']/a").text + " comments | " + 
          nw.at_css("/div[@class='entry unvoted']/div/span[1]/span")
-    time = nw.at_css("/div[@class='entry unvoted']/div/span[1]/time")
-    timeutc = nw.at_css("div[@class='entry unvoted']/div/span[1]/time/@title")
-    puts "Time posted: #{time.text} | " + "#{timeutc.text}"
+        time = nw.at_css("/div[@class='entry unvoted']/div/span[1]/time")
+        timeutc = nw.at_css("div[@class='entry unvoted']/div/span[1]/time/@title")
+        puts "Time posted: #{time.text} | " + "#{timeutc.text}"
+        puts " "
+    end
+    
+    when "popular"
+    
+    dochot = Nokogiri::HTML(open("https://i.reddit.com/r/buildapcsales"))
+    dochot.remove_namespaces!
+    hotsales = dochot.xpath("//div[@class='content']/div[@id='siteTable']/div")
+    
     puts " "
+    puts "Popular Deals"
+    puts "============="
+    puts " "
+    hotsales[1..-1].each do |sl|
+        count+=1
+        puts sl.at_css('p/a').text
+        slink = sl.at_css("p[@class='title']/a/@href")
+        link = sl.at_css('@href')
+        puts "Store link: #{slink.text}"
+        puts "Reddit thread link:  #{link.text}"
+        puts sl.at_css("div[@class='commentcount']/a").text + " comments | " + 
+        sl.at_css("/div[@class='entry unvoted']/div/span[1]/span")
+        time = sl.at_css("/div[@class='entry unvoted']/div/span[1]/time")
+        timeutc = sl.at_css("/div[@class='entry unvoted']/div/span[1]/time/@title")
+        puts "Time posted: #{time.text} | " + "#{timeutc.text}"
+        puts " "
+    end
+    else
+        puts "Invalid choice"
 end
 
-puts "Popular Deals"
-puts "============="
-puts " "
 
-dochot = Nokogiri::HTML(open("https://i.reddit.com/r/buildapcsales"))
-dochot.remove_namespaces!
-hotsales = dochot.xpath("//div[@class='content']/div[@id='siteTable']/div")
-
-hotsales[1..-1].each do |sl|
-    count+=1
-    puts sl.at_css('p/a').text
-    slink = sl.at_css("p[@class='title']/a/@href")
-    link = sl.at_css('@href')
-    puts "Store link: #{slink.text}"
-    puts "Reddit thread link:  #{link.text}"
-    puts sl.at_css("div[@class='commentcount']/a").text + " comments | " + 
-         sl.at_css("/div[@class='entry unvoted']/div/span[1]/span")
-    time = sl.at_css("/div[@class='entry unvoted']/div/span[1]/time")
-    timeutc = sl.at_css("/div[@class='entry unvoted']/div/span[1]/time/@title")
-    puts "Time posted: #{time.text} | " + "#{timeutc.text}"
-    puts " "
-end
 
 puts "#{count} sales found"
 puts " "
